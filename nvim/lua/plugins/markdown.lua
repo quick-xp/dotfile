69789b83a436fe0d2f77e95b61575d08d61c3890
rnishio <ryosuke.nishio@spacemarket.co.jp>
2026-01-07 18:59:36 +0900

    移行

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,17 +1,25 @@
 -- Markdown プレビュー（左右分割）
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
+  -- markview.nvim を使用
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      preview = {
+        -- デフォルトでは通常表示（プレビューしない）
+        enable = false,
+      },
+    },
+    keys = {
+      { "<leader>mp", "<cmd>Markview splitToggle<cr>", desc = "Markdown Preview Split" },
+      { "<leader>mt", "<cmd>Markview Enable<cr>", desc = "Markdown Enable Render" },
+    },
   },
-  opts = {
-    -- デフォルトでは通常表示（プレビューしない）
-    initial_state = false,
-  },
-  keys = {
-    { "<leader>mp", "<cmd>Markview splitToggle<cr>", desc = "Markdown Preview Split" },
-    { "<leader>mt", "<cmd>Markview toggle<cr>", desc = "Markdown Toggle Render" },
-  },
+  -- 干渉するプラグインを無効化
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
 }
