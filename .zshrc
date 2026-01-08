458d08df3c1f8dec03be66ca69c06487c4f5dbcd
rnishio <ryosuke.nishio@spacemarket.co.jp>
2026-01-08 21:56:08 +0900

    dev

--- .zshrc
+++ .zshrc
@@ -176,3 +176,6 @@ fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# mcat で tmux 内でも画像表示
+alias mcat="mcat --iterm"
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -28,9 +28,39 @@ return {
         desc = "Markdown Preview Fullscreen",
       },
       { "<leader>mt", "<cmd>Markview Enable<cr>", desc = "Markdown Enable Render" },
+      {
+        "<leader>mm",
+        function()
+          -- 現在のファイルを保存
+          vim.cmd("silent! write")
+          local filepath = vim.fn.expand("%:p")
+          -- tmux で右側にペインを開いて mcat-mermaid を実行
+          local cmd = string.format(
+            "tmux split-window -h 'mcat-mermaid %s; read -n 1'",
+            vim.fn.shellescape(filepath)
+          )
+          vim.fn.system(cmd)
+        end,
+        desc = "Mermaid Preview (mcat)",
+        ft = "markdown",
+      },
     },
   },
   -- 干渉するプラグインを無効化
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
-  { "iamcco/markdown-preview.nvim", enabled = false },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = ":call mkdp#util#install()",
+    init = function()
+      -- Safari でプレビュー（普段使わないから専用にできる）
+      vim.g.mkdp_browser = "safari"
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_combine_preview = 0
+    end,
+    keys = {
+      { "<leader>mb", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview (Browser)" },
+    },
+  },
 }
